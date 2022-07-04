d34f00cf249bc84842d05b55f3b5066d6947a763
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2022-07-04 23:30:37 +0530

    first commit

---
+++ july3rd.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class july3rd {
+    
+        public int wiggleMaxLength(int[] nums) {
+            if(nums.length <= 1){
+                return nums.length;
+            }
+            int up = 1;
+            int down = 1;
+            for(int i=0;i<nums.length-1;i++){
+                if(nums[i] < nums[i+1]){
+                    up = down + 1;
+                }else if(nums[i] > nums[i+1]){
+                    down = up + 1;
+                }
+            }
+            return Math.max(up,down);
+        }
+    }
+
